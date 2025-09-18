fcd31ea7e429081ade26985104bcab11049846d9
urgirlllbelleee2005 <castriel21@gmail.com>
2025-09-17 21:06:46 -0800

    Update main.dart
    updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
 // lib/main.dart
 import 'vehicle.dart';
+import 'car.dart';
 
 void main() {
   print('--- Vehicle Demo ---');
-
   var v = Vehicle(brand: 'Generic', year: 2000, startingSpeed: 10);
   print(v.info());
   v.accelerate(20);
   v.brake(5);
+
+  print('\n--- Car Demo ---');
+  var c = Car(brand: 'Toyota', year: 2022, passengers: 5, startingSpeed: 0);
+  print(c.info());
+  c.honk();
+  c.accelerate(15);
+  c.brake(10);
 }
